ae87292493f4d5dc59a76209ecfa89cb15235af8
dbelett98 <dbelett@outlook.com>
2025-01-26 20:20:06 -0600

    tried the Person Class from the Week 2 videos

--- msse-661/week-two/app.js
+++ msse-661/week-two/app.js
@@ -1,1 +1,16 @@
 console.log("Hello Professor!")
+
+
+// following code is from the week 2 videos
+
+class Person {
+    name;
+    age;
+
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+console.log(new Person('David', 26));
