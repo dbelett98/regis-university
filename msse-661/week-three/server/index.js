96a08597331c8615e4f5e0c53b5a766c529637b9
dbelett98 <dbelett@outlook.com>
2025-02-02 19:00:04 -0600

    added middleware function localhost that runs my html/css code when starting the local web server

--- msse-661/week-three/server/index.js
+++ msse-661/week-three/server/index.js
@@ -2,6 +2,13 @@
 const express = require('express');
 const app = express();
 
+
+
+// middleware -- defines how things should happen before output executes
+app.use(express.static('public'));
+
+
+
 app.listen(3000, function(){
     console.log("Server started at http://localhost:%s", 3000);
 });
