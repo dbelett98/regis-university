4ae60727660c947cbf49ffa65512c25aa40f0f5f
dbelett98 <dbelett@outlook.com>
2025-02-09 17:22:22 -0600

    followed Professor Morgans isntructions for index.js

--- msse-661/week-four/server/index.js
+++ msse-661/week-four/server/index.js
@@ -1,20 +1,38 @@
-// following Professor Morgan's video
 const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const authRoutes = require('./routes/auth.routes');
+const userRoutes = require('./routes/user.routes');
+const tasksRoutes = require('./routes/tasks.routes');
+const middleware = require('./middleware/errors.middleware');
+
 const app = express();
+const port = process.env.PORT || 3000;
+const logLevel = process.env.LOG_LEVEL || 'dev';
 
+// Middleware - logs server requests to console
+app.use(logger(logLevel));
 
+// Middleware - parses incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// middleware -- defines how things should happen before output executes
-app.use(express.static('public'));
+// Serve static files from the "public" directory
+app.use(express.static('../public')); 
 
-app.use('/css', express.static(__dirname + '/public/css'));
+// API endpoints
+app.use('/api/auth', authRoutes); // http://localhost:3000/api/auth
+app.use('/api/user', userRoutes); // http://localhost:3000/api/users
+app.use('/api/tasks', tasksRoutes); // http://localhost:3000/api/tasks
 
-app.listen(3000, function(){
-    console.log("Server started at http://localhost:%s", 3000);
+// Handle 404 requests
+app.use(middleware.error404);
+
+// Handle 500 requests - applies mostly to live services
+app.use(middleware.error500);
+
+// Listen on server port
+app.listen(port, function() {
+  console.log(`Running on port: http://localhost:${port}...`);
 });
-
-
-
-
-// console message
-console.log("Hello Professor Jelena!");
