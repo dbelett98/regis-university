c9f41817eecc29906ae862a9910aa9ef3756012d
dbelett98 <dbelett@outlook.com>
2025-03-01 11:04:56 -0600

    created server script

---
+++ msse-661/project/server/server.js
@@ -0,0 +1,11 @@
+//  creating server script
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+  console.log(`Web server is running at http://localhost:${PORT}`);
+});
